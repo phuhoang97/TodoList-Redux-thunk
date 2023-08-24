3678ee1934a18f5d2ab9c045feaad420b14e8f69
phuhoang97 <59466061+phuhoang97@users.noreply.github.com>
2023-08-24 08:34:50 +0700

    upload

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchTodos,
+  addTodo,
+  updateTodo,
+  deleteTodo,
+  selectAllTodos,
+  selectTodoStatus,
+} from "../redux/todoSlice";
+
+function TodoList() {
+  const dispatch = useDispatch();
+  const todos = useSelector(selectAllTodos);
+  const todoStatus = useSelector(selectTodoStatus);
+
+  const [newTodoTitle, setNewTodoTitle] = useState("");
+
+  useEffect(() => {
+    if (todoStatus === "idle") {
+      dispatch(fetchTodos());
+    }
+  }, [todoStatus, dispatch]);
+
+  const handleAddTodo = async () => {
+    if (newTodoTitle) {
+      const newTodo = {
+        title: newTodoTitle,
+        completed: false,
+        userId: 1,
+      };
+
+      await dispatch(addTodo(newTodo));
+      setNewTodoTitle("");
+    }
+  };
+
+  const handleToggleComplete = async (todo) => {
+    const updatedTodo = { ...todo, completed: !todo.completed };
+    await dispatch(updateTodo(updatedTodo));
+  };
+
+  const handleDeleteTodo = async (todoId) => {
+    await dispatch(deleteTodo(todoId));
+  };
+
+  if (todoStatus === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (todoStatus === "failed") {
+    return <div>Error: Cannot fetch todos</div>;
+  }
+
+  return (
+    <div>
+      <h1>Todos</h1>
+      <input
+        type='text'
+        placeholder='Enter a new todo'
+        value={newTodoTitle}
+        onChange={(e) => setNewTodoTitle(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+              onClick={() => handleToggleComplete(todo)}
+            >
+              {todo.title}
+            </span>
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
---
+++ src/redux/todoSlice.js
@@ -0,0 +1,81 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  todos: [],
+  status: "idle",
+  error: null,
+};
+
+export const fetchTodos = createAsyncThunk("todos/fetchTodos", async () => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/todos"
+  );
+  return response.data;
+});
+
+export const addTodo = createAsyncThunk("todos/addTodo", async (newTodo) => {
+  const response = await axios.post(
+    "https://jsonplaceholder.typicode.com/todos",
+    newTodo
+  );
+  return response.data;
+});
+
+export const updateTodo = createAsyncThunk(
+  "todos/updateTodo",
+  async (updatedTodo) => {
+    const response = await axios.put(
+      `https://jsonplaceholder.typicode.com/todos/${updatedTodo.id}`,
+      updatedTodo
+    );
+    return response.data;
+  }
+);
+
+export const deleteTodo = createAsyncThunk(
+  "todos/deleteTodo",
+  async (todoId) => {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+    return todoId;
+  }
+);
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTodos.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchTodos.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.todos = action.payload;
+      })
+      .addCase(fetchTodos.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(addTodo.fulfilled, (state, action) => {
+        state.todos.push(action.payload);
+      })
+      .addCase(updateTodo.fulfilled, (state, action) => {
+        const updatedIndex = state.todos.findIndex(
+          (todo) => todo.id === action.payload.id
+        );
+        if (updatedIndex !== -1) {
+          state.todos[updatedIndex] = action.payload;
+        }
+      })
+      .addCase(deleteTodo.fulfilled, (state, action) => {
+        state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+      });
+  },
+});
+
+export default todoSlice.reducer;
+
+export const selectAllTodos = (state) => state.todos.todos;
+export const selectTodoStatus = (state) => state.todos.status;
